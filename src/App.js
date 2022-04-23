7d797fe12ccf4cd7ebbe6f52531f6dfa9d8e737a
abhi-j <37021060+abhi-j@users.noreply.github.com>
2022-04-23 23:40:03 +0530

    implemented sort

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from "react";
+import { BiCalendar } from "react-icons/bi";
+import AddAppointments from "./components/AddAppointments";
+import Search from "./components/Search";
+import Appointmentinfo from "./components/AppointmentInfo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc");
+
+
+ //This array shows the filtered list. We created this beacuse we dont want to disturb previous array
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) || 
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) || 
+        item.aptNotes.toLowerCase().includes(query.toLowerCase()) )
+    }
+  ).sort((a,b) => {
+    let order = (orderBy === 'asc') ? 1 : -1;
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+    ? -1 * order : 1 * order
+    )
+  })
+  const fetchData = useCallback(()=> {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(data=> 
+      {setAppointmentList(data)
+    });
+  }, [])
+
+  useEffect(() => {
+    fetchData()}, [fetchData]
+  )
+
+ // We are using Js FetchApi 
+ //We issue a promise method and say that our data will be retrived
+ //then we are receiving the response from the server we are asking for
+ //arrow function takes response from server and converts it into json 
+ //then we use another promise where we will recive all data 
+ //We are receiving data and asking useCallback to monitor changes that happen to that data  
+ 
+
+
+ return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top" />
+        Book Your Appointments
+      </h1>
+      <Search  query={query} 
+      onQueryChange = {myQuery => setQuery(myQuery)}
+
+      // query will get pass along our local variable and 
+      // then onQueryChange its going to receive something from event 
+      // and then use the setQuery method and pass along what we receive
+/>
+      <AddAppointments />
+      <ul className="divide-y divide-gray-200">
+        {" "}
+        {/* //Importing json */}
+        {filteredAppointments.map((appointment) => (
+          <Appointmentinfo 
+          key={appointment.id} 
+          appointment={appointment}
+          onDeleteAppointment = {
+            appointmentId => setAppointmentList(appointmentList
+              .filter(appointment => appointment.id !== appointmentId)
+            )} />
+        ))}
+      </ul>
     </div>
   );
 }
